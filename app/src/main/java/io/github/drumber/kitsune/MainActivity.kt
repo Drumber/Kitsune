32f21916ccabe963a9b36a1b970e5d9c6aa7c66e
Lars <29163322+Drumber@users.noreply.github.com>
2021-09-10 14:29:05 +0200

    Created android studio project

---
+++ app/src/main/java/io/github/drumber/kitsune/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.github.drumber.kitsune
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.kitsune.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
