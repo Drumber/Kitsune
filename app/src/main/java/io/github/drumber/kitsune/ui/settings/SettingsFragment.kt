4ef97f215ecf7b6af1c5d9365d1c0a8ef02144fa
Lars <29163322+Drumber@users.noreply.github.com>
2021-11-07 15:57:54 +0100

    Added about category to settings

--- app/src/main/java/io/github/drumber/kitsune/ui/settings/SettingsFragment.kt
+++ app/src/main/java/io/github/drumber/kitsune/ui/settings/SettingsFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.View
 import androidx.navigation.fragment.findNavController
 import androidx.preference.ListPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import io.github.drumber.kitsune.BuildConfig
 import io.github.drumber.kitsune.R
+import io.github.drumber.kitsune.data.model.TitlesPref
 import io.github.drumber.kitsune.databinding.FragmentPreferenceBinding
 import io.github.drumber.kitsune.preference.KitsunePref
-import io.github.drumber.kitsune.data.model.TitlesPref
 import io.github.drumber.kitsune.util.initPaddingWindowInsetsListener
 
 class SettingsFragment : PreferenceFragmentCompat() {
@@ -16,16 +18,18 @@ class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.app_preferences, rootKey)
 
-        val titlesPref = findPreference<ListPreference>(getString(R.string.preference_key_titles))
-        titlesPref?.apply {
+        findPreference<ListPreference>(getString(R.string.preference_key_titles))?.apply {
             entryValues = TitlesPref.values().map { it.name }.toTypedArray()
             setDefaultValue(KitsunePref.titles.name)
             value = KitsunePref.titles.name
-            setOnPreferenceChangeListener { preference, newValue ->
+            setOnPreferenceChangeListener { _, newValue ->
                 KitsunePref.titles = TitlesPref.valueOf(newValue.toString())
                 true
             }
         }
+
+        val appVersion = "${BuildConfig.VERSION_NAME} (${BuildConfig.VERSION_CODE})"
+        findPreference<Preference>(getString(R.string.preference_key_app_version))?.summary = appVersion
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
