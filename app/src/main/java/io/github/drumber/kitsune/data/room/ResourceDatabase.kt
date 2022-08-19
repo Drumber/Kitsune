e9ad95e7f3f834d2c62d38bf93fb6acc2e34220a
Lars <29163322+Drumber@users.noreply.github.com>
2022-08-19 19:18:56 +0200

    Added database migration
    Delete table `offline_library_update`

--- app/src/main/java/io/github/drumber/kitsune/data/room/ResourceDatabase.kt
+++ app/src/main/java/io/github/drumber/kitsune/data/room/ResourceDatabase.kt
@@ -1,8 +1,7 @@
 package io.github.drumber.kitsune.data.room
 
-import androidx.room.Database
-import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
+import androidx.room.*
+import androidx.room.migration.AutoMigrationSpec
 import io.github.drumber.kitsune.data.model.RemoteKey
 import io.github.drumber.kitsune.data.model.library.LibraryEntry
 import io.github.drumber.kitsune.data.model.library.LibraryModification
@@ -10,7 +9,10 @@ import io.github.drumber.kitsune.data.model.library.LibraryModification
 @Database(
     entities = [LibraryEntry::class, RemoteKey::class, LibraryModification::class],
     version = 25,
-    exportSchema = true
+    exportSchema = true,
+    autoMigrations = [
+        AutoMigration (from = 24, to = 25, spec = ResourceDatabase.MigrationSpec_24_25::class)
+    ]
 )
 @TypeConverters(Converters::class)
 abstract class ResourceDatabase : RoomDatabase() {
@@ -18,5 +20,8 @@ abstract class ResourceDatabase : RoomDatabase() {
     abstract fun libraryEntryDao(): LibraryEntryDao
     abstract fun remoteKeys(): RemoteKeyDao
     abstract fun offlineLibraryModificationDao(): OfflineLibraryModificationDao
+
+    @DeleteTable(tableName = "offline_library_update")
+    class MigrationSpec_24_25 : AutoMigrationSpec
 
 }
