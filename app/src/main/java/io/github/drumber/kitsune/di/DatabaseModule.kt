58b45add988b3be8826448fd3f13d12f33e9d094
Lars <29163322+Drumber@users.noreply.github.com>
2023-09-30 13:07:35 +0200

    Added migration for deleting database files from version <= 1.7.8

--- app/src/main/java/io/github/drumber/kitsune/di/DatabaseModule.kt
+++ app/src/main/java/io/github/drumber/kitsune/di/DatabaseModule.kt
@@ -3,7 +3,6 @@ package io.github.drumber.kitsune.di
 import android.app.Application
 import androidx.room.Room
 import io.github.drumber.kitsune.domain.database.LocalDatabase
-import io.github.drumber.kitsune.domain.room.ResourceDatabase
 import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module
 
@@ -13,16 +12,10 @@ val databaseModule = module {
     factory { get<LocalDatabase>().libraryEntryModificationDao() }
     factory { get<LocalDatabase>().libraryEntryWithModification() }
     factory { get<LocalDatabase>().remoteKeyDao() }
-    single { createResourceDatabase(androidApplication()) }
 }
 
 private fun createLocalDatabase(application: Application): LocalDatabase {
     return Room.databaseBuilder(application, LocalDatabase::class.java, "kitsune.db")
         .fallbackToDestructiveMigration()
-        .build()
-}
-
-private fun createResourceDatabase(application: Application): ResourceDatabase {
-    return Room.databaseBuilder(application, ResourceDatabase::class.java, "resources.db")
         .build()
 }
