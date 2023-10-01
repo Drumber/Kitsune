cbdd2267cf0ef71474f81a9b86de2d0a93fe183a
Lars <29163322+Drumber@users.noreply.github.com>
2023-10-01 11:43:37 +0200

    Include header with build and device information in log files

--- app/src/main/java/io/github/drumber/kitsune/util/LogCatReader.kt
+++ app/src/main/java/io/github/drumber/kitsune/util/LogCatReader.kt
@@ -1,8 +1,11 @@
 package io.github.drumber.kitsune.util
 
+import android.os.Build
+import io.github.drumber.kitsune.BuildConfig
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.File
+import java.util.Date
 
 object LogCatReader {
 
@@ -13,14 +16,32 @@ object LogCatReader {
         }
     }
 
-    suspend fun writeAppLogsToFile(file: File) = withContext(Dispatchers.IO) {
+    suspend fun writeAppLogsToFile(
+        file: File,
+        writeHeader: Boolean = true
+    ) = withContext(Dispatchers.IO) {
         val logs = readAppLogs()
         file.parentFile?.mkdirs()
         file.bufferedWriter().use { writer ->
+            if (writeHeader)
+                writer.appendLine(generateLogFileHeader())
             logs.forEach { line ->
                 writer.appendLine(line)
             }
         }
+    }
+
+    private fun generateLogFileHeader(): String {
+        return StringBuilder()
+            .appendLine("###########################################################")
+            .appendLine("Log file generated on ${Date()}")
+            .appendLine("Kitsune version: ${BuildConfig.VERSION_NAME} (${BuildConfig.VERSION_CODE})")
+            .appendLine("Application ID: ${BuildConfig.APPLICATION_ID}")
+            .appendLine("Build type: ${BuildConfig.BUILD_TYPE}")
+            .appendLine("Android version: ${Build.VERSION.RELEASE} (SDK ${Build.VERSION.SDK_INT})")
+            .appendLine("Device: ${Build.MANUFACTURER} ${Build.MODEL}")
+            .appendLine("###########################################################")
+            .toString()
     }
 
 }
