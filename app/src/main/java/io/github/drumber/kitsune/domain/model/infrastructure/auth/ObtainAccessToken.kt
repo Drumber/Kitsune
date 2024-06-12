4b9af13a68ecc9ce9ec72cded5d50c76b399ddbc
Lars <29163322+Drumber@users.noreply.github.com>
2024-06-12 17:35:25 +0200

    Do not encode password when obtaining access token (#51) Although the API docs state that the password must be an RFC3986 URl encoded string, the password must actually be provided as a plain string. Ref: https://hummingbird-me.github.io/api-docs/#section/Authentication/Obtaining-an-Access-Token

--- app/src/main/java/io/github/drumber/kitsune/domain/model/infrastructure/auth/ObtainAccessToken.kt
+++ app/src/main/java/io/github/drumber/kitsune/domain/model/infrastructure/auth/ObtainAccessToken.kt
@@ -1,14 +1,12 @@
 package io.github.drumber.kitsune.domain.model.infrastructure.auth
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import java.net.URLEncoder
 
 data class ObtainAccessToken(
     @JsonProperty("grant_type")
     val grantType: String = "password",
     @JsonProperty("username")
     val username: String,
-    /** RFC3986 URl encoded string */
     @JsonProperty("password")
     val password: String
 ) {
@@ -17,7 +15,7 @@ data class ObtainAccessToken(
         fun build(username: String, password: String): ObtainAccessToken {
             return ObtainAccessToken(
                 username = username,
-                password = URLEncoder.encode(password, "UTF-8")
+                password = password
             )
         }
     }
