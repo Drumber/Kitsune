79a517902657846641b94856b810c749e0081db7
Lars <29163322+Drumber@users.noreply.github.com>
2024-11-18 18:23:54 +0100

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "io.github.drumber.kitsune"
         minSdk = 26
         targetSdk = 35
-        versionCode = 36
-        versionName = "2.0.3"
+        versionCode = 37
+        versionName = "2.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
