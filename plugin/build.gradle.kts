ef69695a7761a06737077b21d34579e4218e8831
Lars <29163322+Drumber@users.noreply.github.com>
2025-02-23 11:44:27 +0100

    Fix build error Fix gradle error: Inconsistent JVM-target compatibility

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,11 +1,19 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("java-gradle-plugin")
     alias(libs.plugins.jetbrains.kotlin.jvm)
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_11
+    }
 }
 
 dependencies {
