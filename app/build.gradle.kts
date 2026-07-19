7428d46865779b9a187a19668509c1e82307d028
Lars <29163322+Drumber@users.noreply.github.com>
2026-07-19 19:38:39 +0200

    Bump version and disable R8 full R8 full mode caused runtime errors in the release build. Since I don't want to fiddle around with proguard rules, I leave full mode disabled for now.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "io.github.drumber.kitsune"
         minSdk = 26
         targetSdk = 36
-        versionCode = 39
-        versionName = "2.0.6"
+        versionCode = 40
+        versionName = "2.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
